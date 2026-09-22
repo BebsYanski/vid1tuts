670bb1a891fbb751ed6cf5a75b8447733a8aedf9
BebsYanski <bebongnchu.yannick@isparkinno.com>
2026-09-22 15:44:27 +0100

    Add comments endpoint to fetch comments for a specific blog

--- main.py
+++ main.py
@@ -11,8 +11,20 @@ def index():
 
     return {"data": "blog list"}
 
+@app.get("blog")
+def func():
 
 @app.get("/blog/{blog_id}")
 def show(blog_id: int):
     """Show individual blog"""
     return {"data": f"blog {blog_id} fetched"}
+
+
+@app.get("/blog/{blog_id}/comments")
+def comments(blog_id: int):
+    """Returns comments for a single blog"""
+
+    return {
+        "message": f"Comments for blog {blog_id}",
+        "data": [{"1": "Comment 1", "2": "Comment 2", "3": "Comment 3"}],
+    }
