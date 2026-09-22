a4a55c86e9157e462c8b7993b82e1fd9ca3e3206
BebsYanski <bebongnchu.yannick@isparkinno.com>
2026-09-22 16:38:45 +0100

    Add Blog model and enhance unpublished blogs endpoint with sorting parameter

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 """My main file"""
 
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Models
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = None
+    updated: bool | None = None
+
+
+# Paths or Routes (Endpoints)
 
 
 @app.get("/")
@@ -17,7 +32,7 @@ def index(limit: int = 10, published: bool = True, sort: str | None = None):
 
 # Should always be above the dynamic route of the same kind.
 @app.get("/blog/unpublished")
-def unpublished():
+def unpublished(sort: Optional[str] = None):
     """Fetch all unpublished blogs"""
 
     return {"data": "Unpublished blog list"}
@@ -26,6 +41,7 @@ def unpublished():
 @app.get("/blog/{blog_id}")
 def show(blog_id: int):
     """Show individual blog"""
+
     return {"data": f"blog {blog_id} fetched"}
 
 
@@ -37,3 +53,13 @@ def comments(blog_id: int):
         "message": f"Comments for blog {blog_id}",
         "data": [{"1": "Comment 1", "2": "Comment 2", "3": "Comment 3"}],
     }
+
+
+# Request body
+
+
+@app.post("/blog")
+def create_blog(data: Blog):
+    """Create a new blog"""
+
+    return data
