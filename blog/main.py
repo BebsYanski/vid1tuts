7e4b8960a1c76c31ddb3b09b30135b9a7f239063
BebsYanski <bebongnchu.yannick@isparkinno.com>
2026-09-23 16:48:59 +0100

    Refactor blog models and update creation logic to enhance data validation and response structure

--- blog/main.py
+++ blog/main.py
@@ -3,10 +3,10 @@
 from contextlib import asynccontextmanager
 from typing import Annotated, Generator
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from sqlmodel import SQLModel, Session
 
-from blog.schemas import Blog
+from blog.schemas import Blog, BlogCreate, BlogPublic
 from blog.database import engine
 
 
@@ -34,10 +34,15 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.post("/blog")
-def create_blog(data: Blog, session: SessionDep):
+@app.post(
+    "/blog",
+    response_model=BlogPublic,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_blog(data: BlogCreate, session: SessionDep) -> Blog:
     """Blog creation function"""
-    session.add(data)
+    blog = Blog.model_validate(data)
+    session.add(blog)
     session.commit()
-    session.refresh(data)
-    return data
+    session.refresh(blog)
+    return blog
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,8 +1,50 @@
-from pydantic import BaseModel
+"""Models for blog posts."""
+
+import datetime
+from typing import Optional
+
+from sqlmodel import SQLModel, Field
 
 
-class Blog(BaseModel):
-    """Contains fields for creating a blog"""
+# Best for timezone aware datetime objects, but requires Python 3.9+
+def utc_now() -> datetime.datetime:
+    """Timezone-aware UTC now."""
+    return datetime.datetime.now(datetime.UTC)
 
-    title: str
+
+class BlogBase(SQLModel):
+    """Base model for blog posts."""
+
+    title: str = Field(
+        index=True
+    )  # index=True for faster searches. Use when you want to filter by this field
     body: str
+    published: bool = Field(default=False, index=True)
+    created_at: datetime.datetime = Field(default_factory=utc_now)
+    updated_at: Optional[datetime.datetime] = None
+
+
+class Blog(BlogBase, table=True):
+    """Blog model for database table."""
+
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class BlogCreate(BlogBase):
+    """Create model for blog posts."""
+
+    pass
+
+
+class BlogPublic(BlogBase):
+    """Public model for blog posts."""
+
+    id: int
+
+
+class BlogUpdate(SQLModel):
+    """Update model for blog posts."""
+
+    title: str | None = None
+    body: str | None = None
+    published: bool | None = None
